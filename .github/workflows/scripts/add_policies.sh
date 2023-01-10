d1915eb9f148a288aeb6a2214c6a6ca8d80b4d4e
Jameleddine Abdelhafidh <jamelabdelhafidh@hotmail.com>
2023-01-10 16:25:04 +0100

    testest

--- .github/workflows/scripts/add_policies.sh
+++ .github/workflows/scripts/add_policies.sh
@@ -6,7 +6,7 @@ BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 git config --global user.email "jamelabdelhafidh@hotmail.com"
 git config --global user.name "XammaXJammaJ"
 
-GH_TOKEN=${{ secrets.GITHUB_TOKEN }}
+GH_TOKEN=$1
 echo "TOKEN:"
 echo $GH_TOKEN
 
