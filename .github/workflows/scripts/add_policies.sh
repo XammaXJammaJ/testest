afaf54f69c89a7ebbec528d928083104ed69d570
Jameleddine Abdelhafidh <jamelabdelhafidh@hotmail.com>
2023-01-10 15:18:13 +0100

    testest

---
+++ .github/workflows/scripts/add_policies.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Add policies to the current branch
+
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+git config --global user.email "jamelabdelhafidh@hotmail.com"
+git config --global user.name "XammaXJammaJ"
+
+
+echo "Showing policies"
+curl -X GET https://api.github.com/repos/XammaXJammaJ/testest/branches/${BRANCH_NAME}/protection/required_pull_request_reviews \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer 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"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+
+echo "Setting policy"
+# Add policy 1: Require pull request before merging
+# ...
+curl -X PUT https://api.github.com/repos/XammaXJammaJ/testest/branches/${BRANCH_NAME}/protection/required_pull_request_reviews \
+-H "Authorization: Bearer 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" \
+-H "Content-Type: application/json" \
+-d '{"required_pull_request_reviews":{"require_code_owner_reviews":false}}'
+
+# Add policy 2
+# ...
+
+# Commit and push changes
+git commit -am "Added policies to $BRANCH_NAME"
+git push origin $BRANCH_NAME
